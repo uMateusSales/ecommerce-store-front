15880166ebebcff9fd36aef3001b923b7bfd3af7
Mateus Sales <mateus.lins.sales@gmail.com>
2023-08-21 17:37:52 -0300

    feat: melhoria na ux mobile da lista de produtos

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -15,7 +15,7 @@ const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
       <h3 className="font-bold text-3xl">{title}</h3>
       {items.length === 0 && <NoResults />}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {items.map((i) => (
           <ProductContainer key={i.id} data={i} />
         ))}
